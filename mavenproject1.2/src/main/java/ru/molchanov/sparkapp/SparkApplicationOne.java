c41017681bfd4de55c24c5b11d8050cfae4dbc0f
Pavel1709 <56599291+Pavel1709@users.noreply.github.com>
2020-12-19 22:01:16 +0300

    Add final  version
    build version

---
+++ mavenproject1.2/src/main/java/ru/molchanov/sparkapp/SparkApplicationOne.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.molchanov.sparkapp;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *
+ * @author pavel1709
+ */
+public class SparkApplicationOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SparkFrame sf = new SparkFrame();
+        sf.setVisible(true);
+        sf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+    }
+    
+}
